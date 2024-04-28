901e1f421fdee6c07ca5024cdc3c7937d6be1798
mmccanse <148015402+mmccanse@users.noreply.github.com>
2024-04-27 18:49:03 -0600

    setup Qdrant vector store and code to chunk and embed PDFs

--- test3.py
+++ test3.py
@@ -1,11 +1,11 @@
 
 import os
 import streamlit as st
-from langchain.document_loaders import PyPDFLoader
+from langchain_community.document_loaders import PyPDFLoader
+from langchain_text_splitters import RecursiveCharacterTextSplitter
 import PyPDF2
 from langchain import OpenAI 
 from langchain.chat_models import ChatOpenAI
-from langchain.text_splitter import RecursiveCharacterTextSplitter 
 from langchain.embeddings.openai import OpenAIEmbeddings
 from langchain.vectorstores import Chroma 
 from langchain.chains import ConversationalRetrievalChain, LLMChain
