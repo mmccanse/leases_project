9a0a8a6f93ac89bac78786637ea7c96d3e8abc07
mmccanse <148015402+mmccanse@users.noreply.github.com>
2024-05-01 01:01:59 -0600

    update disclaimer

--- Lease_Assistant.py
+++ Lease_Assistant.py
@@ -235,7 +235,7 @@ def main():
     # st.header('This assistant is still a work in progress. The page number references are not quite dialed in. Thank you for your patience.')
     st.divider()
     st.write('This is a retrieval-assisted chatbot for lease accounting under US GAAP.')
-    st.markdown(f"**Disclaimer:** This assistant cannot give specific accounting advice. It is a learning tool and a proof of concept. It is not intended for commercial use. PLease note that page number references are not currently accurate. For accounting advice, please consult an appropriate professional.")
+    st.markdown(f"**Disclaimer:** This assistant cannot give specific accounting advice. It is a learning tool and a proof of concept. It is not intended for commercial use. PLease note that page number references provided in the response are not currently accurate. For accounting advice, please consult an appropriate professional.")
 
     st.divider()
     
