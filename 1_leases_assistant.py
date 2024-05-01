3ab407ccac52f13204f2fcefcf01c1185b5c1453
mmccanse <148015402+mmccanse@users.noreply.github.com>
2024-04-30 18:18:23 -0600

    lighter button color

--- 1_leases_assistant.py
+++ 1_leases_assistant.py
@@ -188,7 +188,7 @@ def submit_button():
         key="sub",
         css_styles="""
             button {
-                background-color: #a51874;
+                background-color: #b74690;
                 color: #ffffff;
                 border-radius: 20px;
                 }
